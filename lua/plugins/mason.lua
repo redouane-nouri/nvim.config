a5f1ff644c9cbf9c85c3e63dfc5c10a03b9b3b80
Redouane Nouri <nour1r3d1@gmail.com>
2026-06-14 15:15:15 +0100

    feat: add mason-tool-installer.nvim for auto-installing tools

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,13 +1,24 @@
 return {
-  {
-    "mason-org/mason-lspconfig.nvim",
-    version = "*",
-    opts = {
-      ensure_installed = { "clangd" },
-    },
-    dependencies = {
-      { "mason-org/mason.nvim", version = "*", config = true },
-      { "neovim/nvim-lspconfig", version = "*" },
-    },
-  },
+	{
+		"mason-org/mason-lspconfig.nvim",
+		version = "*",
+		opts = {
+			ensure_installed = { "clangd", "lua_ls", "ts_ls" },
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", version = "*", config = true },
+			{ "neovim/nvim-lspconfig", version = "*" },
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		version = "*",
+		opts = {
+			ensure_installed = {
+				"clang-format",
+				"stylua",
+				"prettier",
+			},
+		},
+	},
 }
