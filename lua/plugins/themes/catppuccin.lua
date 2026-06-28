d35dcddfcbd474c7b6e884afdcb1281ead4c3023
Redouane Nouri <nour1r3d1@gmail.com>
2026-06-28 15:59:49 +0100

    feat: add catppuccin theme plugin

---
+++ lua/plugins/themes/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+	"catppuccin/nvim",
+	version = "^2",
+	name = "catppuccin",
+	opts = {
+		-- latte, frappe, macchiato, mocha
+		background = {
+			light = "latte",
+			dark = "frappe",
+		},
+	},
+}
