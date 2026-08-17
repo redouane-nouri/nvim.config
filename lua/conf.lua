e37c422ec94fbe7f2f5d9a1c5f7a68289496ce3f
Redouane Nouri <nour1r3d1@gmail.com>
2026-08-17 17:04:55 +0100

    feat: configure blink.cmp capabilities

--- lua/conf.lua
+++ lua/conf.lua
@@ -1,4 +1,4 @@
-vim.cmd.colorscheme("nord")
+vim.cmd.colorscheme("catppuccin")
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "c", "cpp", "typescript", "javascript", "lua", "markdown" },
@@ -7,4 +7,8 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.wo[0][0].foldmethod = "expr"
 		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 	end,
+})
+
+vim.lsp.config("*", {
+	capabilities = require("blink.cmp").get_lsp_capabilities(),
 })
